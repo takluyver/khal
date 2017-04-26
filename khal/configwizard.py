d2d743519ba24ebaaed94b4a60c3c095d62cd0ed
Thomas Kluyver <thomas@kluyver.me.uk>
2017-04-26 13:09:05 +0100

    Fix indentation

--- khal/configwizard.py
+++ khal/configwizard.py
@@ -27,7 +27,7 @@ from functools import partial
 import json
 from itertools import zip_longest
 from os.path import (expanduser, expandvars, join, normpath, exists, isdir,
-    dirname)
+                     dirname)
 from os import makedirs, environ
 from subprocess import call
 
